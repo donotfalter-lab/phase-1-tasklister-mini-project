2daf54a1919f26c6723f45cbab18cc7c271fe790
donotfalter-lab <hjamyll@yahoo.com>
2026-07-22 12:48:09 -0400

    html is the toughest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const createTaskForm = document.getElementById("create-task-form");
+
+createTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskDescription = event.target.elements["new-task-description"].value;
+
+    buildToDo(taskDescription);
+});
+
+function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    const taskList = document.getElementById("tasks");
+    taskList.append(li);
+}
